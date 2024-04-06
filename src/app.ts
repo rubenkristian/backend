692371e25033544531236c11a0af9d7e84bef2b4
Kristian Ruben Sianturi <rubenkristian96@gmail.com>
2024-04-06 14:50:37 +0700

    update package and tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(process.env.PORT || 3000, () => {
+app.get('/', (_req: Request, _res: Response) => {
+    return _res.send("test");
+});
+
+app.listen(process.env.PORT || 8080, () => {
     
 });
